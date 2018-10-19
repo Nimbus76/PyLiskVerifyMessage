60d7cc060144443f783f55b072f0bcde0f17a7ca
Nimbus76 <32406458+Nimbus76@users.noreply.github.com>
2018-10-18 23:13:12 -0500

    Linting

--- verifyLiskMessageInteractive.py
+++ verifyLiskMessageInteractive.py
@@ -1,22 +1,30 @@
-### To Sign A Message, You may use Lisk Hub by
-### creating a URL in the following format:
-### lisk://sign-message?message=In Lisk We Trust
+"""
+Interactive Verification of Lisk Signed Message.
+
+To Sign A Message, You may use Lisk Hub by
+creating a URL in the following format:
+lisk://sign-message?message=In Lisk We Trust
+
+The pynacl libary is required. Try python -m pip install pynacl
+
+"""
 
 import nacl.encoding
 import nacl.signing
 import binascii
 import sys
 
-def main():
 
+def main():
+    """Message Verification."""
     header()
 
     check_another = True
 
-    while check_another == True:
+    while check_another is True:
 
         try:
-                      
+
             msg = getmessage()
 
             message, public_key, signature = parse_user_signature(msg)
@@ -25,31 +33,33 @@ def main():
 
             lisk_address = getAddressFromPublicKey(public_key)
 
-            check_another = success(message, verified_message, public_key, lisk_address)
+            check_another = success(message, verified_message, public_key,
+                                    lisk_address)
 
         except:
 
             check_another = failure()
 
+
 def header():
-    
+    """Print program header and instructions."""
     print('''
 -----------------------------------------------------
-            _   ___           __                  
-           / | / (_)___ ___  / /_  __  _______    
-          /  |/ / / __ `__ \/ __ \/ / / / ___/    
-         / /|  / / / / / / / /_/ / /_/ (__  )     
+            _   ___           __
+           / | / (_)___ ___  / /_  __  _______
+          /  |/ / / __ `__ \/ __ \/ / / / ___/
+         / /|  / / / / / / / /_/ / /_/ (__  )
         /_/ |_/_/_/ /_/ /_/_.___/\__,_/____/
-     
- _    __          _ ____       
-| |  / /__  _____(_) __/_  __  
-| | / / _ \/ ___/ / /_/ / / /   
-| |/ /  __/ /  / / __/ /_/ /     
-|___/\___/_/  /_/_/  \__, /      
-                    /____/                           
------------------------------------------------------                                   
+
+ _    __          _ ____
+| |  / /__  _____(_) __/_  __
+| | / / _ \/ ___/ / /_/ / / /
+| |/ /  __/ /  / / __/ /_/ /
+|___/\___/_/  /_/_/  \__, /
+                    /____/
+-----------------------------------------------------
           LISK SIGNED MESSAGE VERIFICATION
-          
+
 -----------------------------------------------------
 
 WINDOWS COMMAND:
@@ -72,27 +82,29 @@ Try:  python -m pip install pynacl
 Paste Signed Message Below:
 ''')
 
-def getmessage():
 
+def getmessage():
+    """Get Lisk Signed Message from user."""
     msg = sys.stdin.readlines()
 
     message = ''.join(msg)
- 
-    return message
-    
-def parse_user_signature(message):
 
+    return message
+
+
+def parse_user_signature(message):
+    """Isolate message, public key, and singature from Lisk Signed Message."""
     msgStartIndex = message.find('-----MESSAGE') + 18
 
-    msgEndIndex = message.find('-----PUBLIC') -1
-    
+    msgEndIndex = message.find('-----PUBLIC') - 1
+
     pkStartIndex = message.find('-----PUBLIC') + 21
 
-    pkEndIndex = message.find('-----SIGNATURE') -1
+    pkEndIndex = message.find('-----SIGNATURE') - 1
 
     sigStartIndex = pkEndIndex + 21
 
-    sigEndIndex = message.find('-----END') -1
+    sigEndIndex = message.find('-----END') - 1
 
     msg = message[msgStartIndex:msgEndIndex]
 
@@ -102,38 +114,43 @@ def parse_user_signature(message):
 
     return msg, pk, sig
 
-def verify_message(message, signature, public_key):
 
+def verify_message(message, signature, public_key):
+    """Verify signature against message and public key."""
     msgBytes = digestMessage(message)
 
     signatureBytes = hex_to_bytes(signature)
-    
-    verify_key = nacl.signing.VerifyKey(public_key, encoder=nacl.encoding.HexEncoder)
+
+    verify_key = nacl.signing.VerifyKey(public_key,
+                                        encoder=nacl.encoding.HexEncoder)
 
     verified_message = verify_key.verify(msgBytes, signatureBytes)
 
     return verified_message
 
-def digestMessage(message):
 
+def digestMessage(message):
+    """Prepare and double-hash message."""
     SIGNED_MESSAGE_PREFIX = 'Lisk Signed Message:\n'
 
     SIGNED_MESSAGE_PREFIX_LENGTH = int_to_varint(len(SIGNED_MESSAGE_PREFIX))
 
     SIGNED_MESSAGE_PREFIX_BYTES = bytes(SIGNED_MESSAGE_PREFIX, 'utf-8')
-    
+
     msgBytes = bytes(message, 'utf-8')
 
     msgLenBytes = int_to_varint(len(message))
 
-    dataBytes = SIGNED_MESSAGE_PREFIX_LENGTH + SIGNED_MESSAGE_PREFIX_BYTES + msgLenBytes + msgBytes
+    dataBytes = SIGNED_MESSAGE_PREFIX_LENGTH + SIGNED_MESSAGE_PREFIX_BYTES \
+        + msgLenBytes + msgBytes
 
     hash = nacl.bindings.crypto_hash_sha256
-    
+
     return hash(hash(dataBytes))
 
-def getAddressFromPublicKey(publicKey):
 
+def getAddressFromPublicKey(publicKey):
+    """Get Lisk address from public key in Lisk Signed Message."""
     publicKey = bytes(publicKey, 'utf-8')
 
     publicKey = binascii.unhexlify(publicKey)
@@ -154,17 +171,18 @@ def getAddressFromPublicKey(publicKey):
 
     return liskAddress
 
-def success(message, verified_message, public_key, lisk_address):
 
+def success(message, verified_message, public_key, lisk_address):
+    """Display Success Message and determine exit intention."""
     print()
 
     print('''-----------------------------------------------------
          _____                                __
         / ___/__  _______________  __________/ /
-        \__ \/ / / / ___/ ___/ _ \/ ___/ ___/ / 
-       ___/ / /_/ / /__/ /__/  __(__  |__  )_/  
+        \__ \/ / / / ___/ ___/ _ \/ ___/ ___/ /
+       ___/ / /_/ / /__/ /__/  __(__  |__  )_/
       /____/\__,_/\___/\___/\___/____/____(_)
-      
+
 -----------------------------------------------------
            SIGNATURE SUCCESSFULLY VERIFIED
                 The Signature is Valid
@@ -177,22 +195,21 @@ def success(message, verified_message, public_key, lisk_address):
 -----------------------------------------------------
                Please Vote for Nimbus
            Donate to 12313256070705265970L
-                
+
 -----------------------------------------------------''')
     return check_another_sig()
 
 
 def failure():
-
+    """Display failure message and determine exit intention."""
     print('''-----------------------------------------------------
 
                ______      _ __         __
               / ____/___ _(_) /__  ____/ /
-             / /_  / __ `/ / / _ \/ __  / 
-            / __/ / /_/ / / /  __/ /_/ /  
-           /_/    \__,_/_/_/\___/\__,_/   
-                               
-                                             
+             / /_  / __ `/ / / _ \/ __  /
+            / __/ / /_/ / / /  __/ /_/ /
+           /_/    \__,_/_/_/\___/\__,_/
+
 -----------------------------------------------------
            SIGNATURE VERIFICATION FAILED
             The Signature is Not Valid
@@ -203,8 +220,9 @@ def failure():
 
     return check_another_sig()
 
+
 def check_another_sig():
-    
+    """Determine exit intention."""
     check = input("Check Another Signature? (y/n):")
 
     print()
@@ -212,15 +230,16 @@ def check_another_sig():
     if check.lower() == 'y':
 
         print('Paste Signed Message Below:')
-        
+
         return True
 
     else:
 
         return False
 
-def int_to_varint(val):
 
+def int_to_varint(val):
+    """Convert integer into varible integer."""
     if val < 253:
         return val.to_bytes(1, 'little')
     elif val <= 65535:
@@ -230,14 +249,14 @@ def int_to_varint(val):
     else:
         return b'\xff'+val.to_bytes(8, 'little')
 
-def hex_to_bytes(hexed):
 
+def hex_to_bytes(hexed):
+    """Convert from hexadecimal to bytes."""
     if len(hexed) & 1:
-        
+
         hexed = '0' + hexed
 
     return bytes.fromhex(hexed)
 
-main()
 
-    
+main()
